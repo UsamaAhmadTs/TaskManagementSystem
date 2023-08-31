4ff22bec5e5406073a1eac9d1bd2d519ee79b8db
UsamaAhmadTs <usama.ahmad@techno-soft.com>
2023-08-31 16:07:50 +0500

    jpa 6

--- src/main/java/com/example/TaskManagementApp/server/entities/TaskHistory.java
+++ src/main/java/com/example/TaskManagementApp/server/entities/TaskHistory.java
@@ -1,41 +1,83 @@
-//package com.example.TaskManagementApp.server.entities;
-//import jakarta.persistence.*;
-//import java.time.Instant;
-//@Entity
-//@Table(name = "Task_History")
-//public class TaskHistory {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    @Column(name = "task_history_id")
-//    private Long taskHistoryId;
-//
-//    @Column(name = "TimeStamp",nullable = false)
-//    private Instant timestamp;
-//
-//    @Enumerated(EnumType.STRING)
-//    @Column(name = "OldStatus", nullable = false)
-//    private Task.Status oldStatus;
-//
-//    @Enumerated(EnumType.STRING)
-//    @Column(name = "NewStatus", nullable = false)
-//    private Task.Status newStatus;
-//
+package com.example.TaskManagementApp.server.entities;
+import jakarta.persistence.*;
+import java.time.Instant;
+@Entity
+@Table(name = "Task_History")
+public class TaskHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "task_history_id")
+    private Long taskHistoryId;
+
+    @Column(name = "TimeStamp",nullable = false)
+    private Instant timestamp;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "OldStatus", nullable = false)
+    private Task.Status oldStatus;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "NewStatus", nullable = false)
+    private Task.Status newStatus;
+
+    @ManyToOne
+    @JoinColumn()
+    private User movedBy;
+
 //    @ManyToOne
-//    @JoinColumn()
+//    @JoinColumn(name = "Task_id")
+//    private Task task;
+
+
+//    public Task getTask() {
+//        return task;
+//    }
+//    public void setTask(Task task) {
+//        this.task = task;
+//    }
+
+    public Instant getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Instant timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public Task.Status getOldStatus() {
+        return oldStatus;
+    }
+
+    public void setOldStatus(Task.Status oldStatus) {
+        this.oldStatus = oldStatus;
+    }
+
+    public Task.Status getNewStatus() {
+        return newStatus;
+    }
+
+    public void setNewStatus(Task.Status newStatus) {
+        this.newStatus = newStatus;
+    }
+
+    public User getMovedBy() {
+        return movedBy;
+    }
+    public void setMovedBy(User movedBy) {
+        this.movedBy = movedBy;
+    }
+
+
+}
+
+
+//public class TaskHistory {
+//    private Instant timestamp;
+//    private Task.Status oldStatus;
+//    private Task.Status newStatus;
 //    private User movedBy;
 //
-////    @ManyToOne
-////    @JoinColumn(name = "Task_id")
-////    private Task task;
-//
-//
-////    public Task getTask() {
-////        return task;
-////    }
-////    public void setTask(Task task) {
-////        this.task = task;
-////    }
 //
 //    public Instant getTimestamp() {
 //        return timestamp;
@@ -64,50 +106,8 @@
 //    public User getMovedBy() {
 //        return movedBy;
 //    }
+//
 //    public void setMovedBy(User movedBy) {
 //        this.movedBy = movedBy;
 //    }
-//
-//
 //}
-//
-//
-////public class TaskHistory {
-////    private Instant timestamp;
-////    private Task.Status oldStatus;
-////    private Task.Status newStatus;
-////    private User movedBy;
-////
-////
-////    public Instant getTimestamp() {
-////        return timestamp;
-////    }
-////
-////    public void setTimestamp(Instant timestamp) {
-////        this.timestamp = timestamp;
-////    }
-////
-////    public Task.Status getOldStatus() {
-////        return oldStatus;
-////    }
-////
-////    public void setOldStatus(Task.Status oldStatus) {
-////        this.oldStatus = oldStatus;
-////    }
-////
-////    public Task.Status getNewStatus() {
-////        return newStatus;
-////    }
-////
-////    public void setNewStatus(Task.Status newStatus) {
-////        this.newStatus = newStatus;
-////    }
-////
-////    public User getMovedBy() {
-////        return movedBy;
-////    }
-////
-////    public void setMovedBy(User movedBy) {
-////        this.movedBy = movedBy;
-////    }
-////}
